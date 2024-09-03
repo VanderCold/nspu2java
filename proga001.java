39c41516c2c02c65864e47a90699bb49c1eb5879
VanderCold <vander.cold@mail.ru>
2024-09-03 14:42:38 +0700

    Добавленны условный оператор и оператор цикла

--- proga001.java
+++ proga001.java
@@ -1,5 +1,33 @@
 class proga001 {
 	public static void main(String args[]) {
 		System.out.println("Я такой proga001, блин, class.");
+		//а-ля hello world, вывод текста
+		int Var1 = 1024;
+		int VarRezerv;
+		//System.out.println(VarRezerv); Ничего ещё не присвоено, всё ломается
+		VarRezerv = 411;
+		System.out.println(Var1/2);
+		System.out.println("Целочисленное деление: " + VarRezerv/2);
+		System.out.println("Деление типом данных: " + (double)VarRezerv/2);
+		double Var3 = (double)VarRezerv/2;
+		//Пример того как работать с if :
+
+		//if(Var1=0) ГОВНО, ОТРАВА!!!!!!!!!!!!!!!!!!!!!
+		//	System.out.println("Условние Var1=0 выполнено!");
+		if(Var1/2 == 512)
+                        System.out.println("Условние Var1/2 == 512 выполнено!");
+		if(true)
+                        System.out.println("Условние true выполнено!");
+		System.out.println("Проверяем");
+		System.out.println();
+		//Пример for
+		for (int i=0; i<10; i++)
+			System.out.println("Номер" + i);
+		System.out.println();
+		int j;
+		for (j=0; j<10; j++)
+                        System.out.print(j + " ");
+		//i = 10; Локальная переменная, присутствует чисто в цикле, всё ломает
+		System.out.println();
 	}		
 }
