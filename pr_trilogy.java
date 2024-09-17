71df356464e4ecbb255d435e2dbdddb20d64452e
VanderCold <vander.cold@mail.ru>
2024-09-17 13:30:06 +0700

    Добавлена программа по чтению клавиатуры

---
+++ pr_trilogy.java
@@ -0,0 +1,9 @@
+class pr_trilogy {
+	public static void main(String args[]) throws java.io.IOException {
+		//Чтение с клавиатуры
+		char ch;
+		System.out.print("Введите что-нибудь и нажмите ENTER: ");
+		ch = (char) System.in.read();
+		System.out.println("Вы нажали: " + ch);
+	}
+}
