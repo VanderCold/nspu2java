32d36116c4d390452edee77a3dd28e2c6fb09fca
VanderCold <vander.cold@mail.ru>
2024-10-01 15:04:29 +0700

    Объявили новый экземпляр

--- prIV.java
+++ prIV.java
@@ -7,16 +7,23 @@ class prIV {
 	public static void main(String args[]) {
 		//Создание экземпляра класса
 		Avtomobil lada = new Avtomobil();
-		int rasst;
+		Avtomobil porshe = new Avtomobil();
+		int rasst, rasst2;
 
 		//Присваивание значений внутренним переменным экземпляра класса
 		lada.pass = 4;
 		lada.v = 40;
 		lada.rash = 9.5;
 
+		porshe.pass = 1;
+		porshe.v = 100;
+		porshe.rash = 14.0;
+
 		//Определяем расстояние
 		rasst = (int)(lada.v/lada.rash * 100);
+		rasst2 = (int)(porshe.v/porshe.rash * 100);
 
 		System.out.println("Lada проедет: " + rasst + " км.");
+		System.out.println("Porshe проедет: " + rasst2 + " км.");
 	}
 }
