370fd33907e709ea1684147ff7f24009ee6f7ffd
VanderCold <vander.cold@mail.ru>
2024-10-08 12:57:32 +0700

    Возврат значений через значения

--- prIV.java
+++ prIV.java
@@ -6,7 +6,16 @@ class Avtomobil {
 	void rasst() {
 		System.out.println("Расстояние: " + ((int)(v/rash * 100)));
 	}
+	
+	int rasst2() {
+		return (int)(v/rash*100);
+	}
+
+	double fuelNeeded(int km) {
+		return ((double)km/100)*rash;
+	}
 }
+
 class prIV {
 	public static void main(String args[]) {
 		//Создание экземпляра класса
@@ -17,19 +26,28 @@ class prIV {
 		//Присваивание значений внутренним переменным экземпляра класса
 		lada.pass = 4;
 		lada.v = 40;
-		lada.rash = 9.5;
+		lada.rash = 5.5;
 
 		porshe.pass = 1;
 		porshe.v = 100;
 		porshe.rash = 14.0;
 
+		System.out.println("Для расстояния в 10 км нужно: " + lada.fuelNeeded(10));
+		rasst=10;
+		System.out.println("Для расстояния в 10 км нужно: " + lada.fuelNeeded(rasst));
+
+
 		//Определяем расстояние
 		//rasst = (int)(lada.v/lada.rash * 100);
 		//rasst2 = (int)(porshe.v/porshe.rash * 100);
 
-		System.out.print("Для Lada: ");
+		System.out.println("Для Lada: ");
 		lada.rasst();
+		rasst = lada.rasst2();
+		System.out.println("Результ метода rasst2(): " + rasst);
+
 		System.out.println("Для Porshe: ");
 		porshe.rasst();
+		System.out.println("Результ метода rasst2(): " + porshe.rasst2());
 	}
 }
