9a382ed8e4c222c274d7fc9022c7f41150f15bd6
VanderCold <vander.cold@mail.ru>
2024-10-15 15:23:46 +0700

    Добавили программу с очередью

---
+++ QDeMo.java
@@ -0,0 +1,62 @@
+//Очередь
+class Queue {
+	char[] q;
+	int putloc, getloc; //указатели
+	Queue (int size) {
+		q = new char[size];
+		putloc = getloc = 0;
+		
+	}
+
+	//Метод для помещения символа в очередь
+	void put(char ch) {
+		if(putloc == q.length) {
+			System.out.println(" - очередь переполнена!");
+			return;
+		}
+		q[putloc++] = ch;
+	}
+
+	//Метод для извлечения символа из очереди
+	char get() {
+		if(getloc == putloc) {
+			System.out.println(" - очередь пуста");
+			return (char) 0;
+		}
+		return q[getloc++];
+	}
+}
+
+class QDeMo {
+	public static void main(String[] args) {
+		Queue bigQ = new Queue(100);
+		Queue smallQ = new Queue(4);
+		char ch;
+		int i;
+
+		System.out.println("Использование очереди bigQ для сохранения символов");
+		for (i = 0; i < 26; i++)
+			bigQ.put((char) ('A' + i));
+		//Извлекаем элементы из bigQ
+		System.out.print("Содержимое bigQ: ");
+		for (i = 0; i < 25; i++) {
+			ch = bigQ.get();
+			if (ch != (char) 0)
+				System.out.print(ch);
+		}
+		System.out.println("\n");
+
+		//smallQ
+		for (i = 0; i < 5; i++) {
+			System.out.print("Попытка сохранить " + (char) ('Z' - i));
+			smallQ.put((char) ('Z' - i));
+			System.out.println();
+		}
+		System.out.println();
+		System.out.print("Вывод: ");
+		for (i = 0; i < 5; i++) {
+			ch = smallQ.get();
+			if (ch != (char) 0) System.out.print(ch);
+		}
+	}
+}
