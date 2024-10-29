6187a42084290ec2ac61531b9378db9bd99ea910
VanderCold <vander.cold@mail.ru>
2024-10-29 11:33:17 +0700

    Программа шифровка и дешифровка сообщений

---
+++ prSHESTb.java
@@ -0,0 +1,56 @@
+//Побитывые операции: & | ^ >> >>> << ~
+class prSHESTb {
+	public static void main(String[] args) {
+		char ch;
+		for (int i=0; i < 26; i++) {
+			ch = (char) ('a' + i);
+			System.out.print(ch);
+			ch = (char) ((int) ch & 65503);
+
+			System.out.print(ch + " ");
+		}	
+		System.out.println();
+		
+		//Отоброжение битового представления байта
+		System.out.println();
+		int t;
+		byte val;
+		val = (byte) 'A';
+		for (t = 128; t > 0; t = t / 2) {
+			if ((val & t) != 0) System.out.print("1 ");
+			else System.out.print("0 ");
+		}
+		System.out.println();
+
+		//Установка 6-го бита кодировки символа
+		System.out.println();
+
+		for (int i=0; i < 26; i++) {
+                        ch = (char) ('A' + i);
+                        System.out.print(ch);
+                        ch = (char) ((int) ch | 32);
+
+                        System.out.print(ch + " ");
+                }
+                System.out.println();
+		
+		String msg = "Исходная тестовая строка";
+		String encmsg = "";
+		String decmsg = "";
+		int key = 88;
+
+		System.out.print("Незашифрованное сообщение: ");
+		System.out.println(msg);
+
+		for (int i = 0; i < msg.length(); i++)
+			encmsg = encmsg + (char) (msg.charAt(i) ^ key);
+		System.out.print("Зашифрованное сообщение: ");
+		System.out.println(encmsg);
+
+		for (int i = 0; i < msg.length(); i++)
+			decmsg = decmsg + (char)(encmsg.charAt(i) ^ key);
+
+		System.out.print("Дешифрованное сообщение: ");
+                System.out.println(decmsg);
+	}
+}
