761b354faee8d564ba3e68ed17938e147687acc9
VanderCold <vander.cold@mail.ru>
2024-11-26 11:38:17 +0700

    Добавили примеры полиморфизма

--- pr099.java
+++ pr099.java
@@ -11,12 +11,18 @@ class TwoDShape {
 	TwoDShape(double w, double h) {
 		width = w;
 		height = h;
+		System.out.println("Внутри конструктора 2DSHAPE(double w, double h) ");
 	}
 
 	TwoDShape(double x) {
 		width = height = x;
 	}
 	
+	TwoDShape(TwoDShape ob) {
+		width = ob.width;
+		height = ob.height;
+	}
+
 	void showDim() {
 		System.out.println("Ширина и высота: " + width + " " + height);
 	}
@@ -50,11 +56,17 @@ class Triangle extends TwoDShape {
 	Triangle(String s, double w, double h) {
 		super(w, h);
 		style = s;
+		System.out.println("Внутри конструктора Triangle(String s, double w, double h) ");
 	}
 
 	Triangle(double x) {
 		super(x);
 		style = "закрашенный";
+	}
+
+	Triangle(Triangle ob) {
+		super(ob);
+		style = ob.style;
 	}
 
 	Triangle(int a, int b) {
@@ -69,6 +81,58 @@ class Triangle extends TwoDShape {
 	}
 	void showStyle() {
 		System.out.println("Стиль: " + style);
+	}
+}
+
+//Строгая типизация
+class X {
+	int a;
+	X(int i) {
+		a = i;
+	}
+	void show() {
+		System.out.println("Значение a: " + a);
+	}
+}
+
+class Y {
+	int a;
+	Y(int i) {
+		a = i;
+	}
+}
+
+class Z extends X {
+	int b;
+	Z(int i, int j) {
+		super(j);
+		b = i;
+	}
+	void show() {
+		System.out.println("Значение a и b: " + a + " " + b);
+	}
+}
+
+class ColorTriangle extends Triangle {
+	private String color;
+
+	ColorTriangle (String c, String s, double w, double h) {
+		super(s, w, h);
+		color = c;
+		System.out.println("Внутри конструктора ColorTriangle(String c, String s, double w, double h) ");
+	}
+
+	ColorTriangle (ColorTriangle ob) {
+		super(ob);
+		color = ob.color;
+	}
+
+	String getColor() {
+		return color;
+	}
+
+	void showColor() {
+		System.out.println("Цвет: " + color);
 	}
 }
 
@@ -120,6 +184,10 @@ class pr099 {
 		Triangle t2 = new Triangle("контурный", 8.0, 12.0);
 		Triangle t3 = new Triangle(4.0);
 		Triangle t4 = new Triangle(5, 10);
+		ColorTriangle t5 = new ColorTriangle("синий", "контурный", 8.0, 12.0);
+		ColorTriangle t6 = new ColorTriangle("красный", "закрашенный", 2.0, 5.0);
+		Triangle t7 = new Triangle(t2);
+		ColorTriangle t8 = new ColorTriangle(t5);
 		Rectangle r1 = new Rectangle("сплошная", 4.0, 4.0);
 		Rectangle r2 = new Rectangle(5.0);
 
@@ -165,6 +233,26 @@ class pr099 {
                 System.out.println("Площадь: " + t3.area());
                 System.out.println();
 
+		System.out.println("Информация об объекте t5: ");
+		t5.showStyle();
+		t5.showDim();
+		t5.showColor();
+		System.out.println();
+
+                System.out.println("Информация об объекте t7: ");
+                t7.showStyle();
+                t7.showDim();
+		System.out.println("Площадь: " + t7.area());
+                System.out.println();
+
+		System.out.println("Информация об объекте t8: ");
+                t8.showStyle();
+                t8.showDim();
+		t8.showColor();
+		System.out.println("Площадь: " + t8.area());
+                System.out.println();
+
+
 		System.out.println("Информация об объекте r1: ");
 		r1.showOutline();
                 r1.showDim();
@@ -186,5 +274,26 @@ class pr099 {
 
                 System.out.println("Площадь: " + r2.area());
                 System.out.println();
+		
+		//Строгая типизация
+
+		X x = new X(10);
+		X x2;
+		Y y = new Y(5);
+		Z z = new Z(5,6);
+		x2 = x;
+		x2 = z;
+
+		x.show();
+		System.out.println("При ссылке подкласса");
+		x2.show();
+		x2 = x;
+		System.out.println("При ссылке подкласса");
+		x2.show();
+
+		System.out.println();
+
+		System.out.println("x2.a: " + x2.a);
+		//System.out.println("x2.b: " + x2.b);
 	}
 }
