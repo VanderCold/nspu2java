a095054e57b832e96dc234560825cecdb2d1e756
VanderCold <vander.cold@mail.ru>
2024-12-10 11:38:05 +0700

    Добавили метод получения пред. значения

--- SeriesDemo.java
+++ SeriesDemo.java
@@ -3,13 +3,16 @@ class SeriesDemo {
 		ByTwos ob = new ByTwos();
 		for (int i = 0; i < 0; i++)
 			System.out.println("Следующее значение в последовательности: " + ob.getNext());
+		System.out.println("Предыдущее значение: " + ob.getPrevious());
 		System.out.println("\nСброс последовательности");
 		ob.reset();
 		for (int i = 0; i < 0; i++)
 			System.out.println("Следующее значение в последовательности: " + ob.getNext());
+		System.out.println("Предыдущее значение: " + ob.getPrevious());
 		System.out.println("\nНовое значение");
 		ob.setStart(100);
 		for (int i = 0; i < 0; i++)
                         System.out.println("Следующее значение в последовательности: " + ob.getNext());
+		System.out.println("Предыдущее значение: " + ob.getPrevious());
 	}
 }
