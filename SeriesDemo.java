98d8ed8798f7cf5153bdd3f3fe1503f02b63779e
VanderCold <vander.cold@mail.ru>
2024-12-10 13:09:18 +0700

    Добавили класс B-3 и обновили демо Series

--- SeriesDemo.java
+++ SeriesDemo.java
@@ -1,6 +1,7 @@
 class SeriesDemo {
 	public static void main(String[] args) {
 		ByTwos ob = new ByTwos();
+		ByThrees ob3 = new ByThrees();
 		for (int i = 0; i < 0; i++)
 			System.out.println("Следующее значение в последовательности: " + ob.getNext());
 		System.out.println("Предыдущее значение: " + ob.getPrevious());
@@ -14,5 +15,30 @@ class SeriesDemo {
 		for (int i = 0; i < 0; i++)
                         System.out.println("Следующее значение в последовательности: " + ob.getNext());
 		System.out.println("Предыдущее значение: " + ob.getPrevious());
+
+		System.out.println();
+
+		for (int i = 0; i < 0; i++)
+                        System.out.println("Следующее значение в последовательности: " + ob3.getNext());
+                //System.out.println("Предыдущее значение: " + ob3.getPrevious());
+                System.out.println("\nСброс последовательности");
+                ob3.reset();
+                for (int i = 0; i < 0; i++)
+                        System.out.println("Следующее значение в последовательности: " + ob3.getNext());
+                //System.out.println("Предыдущее значение: " + ob3.getPrevious());
+                System.out.println("\nНовое значение");
+                ob3.setStart(100);
+                for (int i = 0; i < 0; i++)
+                        System.out.println("Следующее значение в последовательности: " + ob3.getNext());
+                //System.out.println("Предыдущее значение: " + ob.getPrevious());
+		
+		Series obInt;
+
+		for (int i = 0; i < 0; i++) {
+			obInt = ob;
+			System.out.println("Следующее значение в последовательности: B-2" + obInt.getNext());
+			obInt = ob3;
+			System.out.println("Следующее значение в последовательности: B-3 " + obInt.getNext());
+		}
 	}
 }
