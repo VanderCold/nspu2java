ac100133de23fdf25f7a234c6ef113b7ef24118f
VanderCold <vander.cold@mail.ru>
2024-12-10 11:26:56 +0700

    Интерфейс и его реализация

--- BookPackExt/pr11.java
+++ BookPackExt/pr11.java
@@ -1,12 +1,14 @@
 package BookPackExt;
+import BookPack.Book;
+//import BookPack.*
 class pr11 {
         public static void main(String[] args) {
-                BookPack.Book[] books = new BookPack.Book[5];
-                books[0] = new BookPack.Book("Иллиада","Гомер",1980);
-                books[1] = new BookPack.Book("Унесенные ветром","Митчелл",2000);
-                books[2] = new BookPack.Book("Наедине с собой","Аврелий",1950);
-                books[3] = new BookPack.Book("Собор Парижской богоматери","Гюго",1970);
-                books[4] = new BookPack.Book("Преступление и наказание","Достоевский",1950);
+                Book[] books = new Book[5];
+                books[0] = new Book("Иллиада","Гомер",1980);
+                books[1] = new Book("Унесенные ветром","Митчелл",2000);
+                books[2] = new Book("Наедине с собой","Аврелий",1950);
+                books[3] = new Book("Собор Парижской богоматери","Гюго",1970);
+                books[4] = new Book("Преступление и наказание","Достоевский",1950);
                 for(int i=0; i < books.length; i++)
                         books[i].show();
         }
---
+++ SeriesDemo.java
@@ -0,0 +1,15 @@
+class SeriesDemo {
+	public static void main(String[] args) {
+		ByTwos ob = new ByTwos();
+		for (int i = 0; i < 0; i++)
+			System.out.println("Следующее значение в последовательности: " + ob.getNext());
+		System.out.println("\nСброс последовательности");
+		ob.reset();
+		for (int i = 0; i < 0; i++)
+			System.out.println("Следующее значение в последовательности: " + ob.getNext());
+		System.out.println("\nНовое значение");
+		ob.setStart(100);
+		for (int i = 0; i < 0; i++)
+                        System.out.println("Следующее значение в последовательности: " + ob.getNext());
+	}
+}
